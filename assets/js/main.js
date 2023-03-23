c7f1a96a90f36477cfe650fca86e01d0407558d5
malkzhub <da.malcolmj@gmail.com>
2023-03-23 20:16:23 +0800

    Updated Functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,53 +1,75 @@
-const options = ["rock", "paper", "scissors"];
-let playerScore = 0;
-let computerScore = 0;
+// // Enter Player Name
+// let playerName = prompt("Enter name: ");
+// const playerInput = document.querySelector("#player-name");
+const options = {
+  82: "rock",
+  80: "paper",
+  83: "scissors",
+  76: "lizard",
+  75: "spock",
+};
 
-function getComputerChoice() {
-  const randomChoice = Math.floor(Math.random() * options.length);
-  return options[randomChoice];
+// playerInput.textContent = playerName;
+
+// function to hightlight what the player selected
+function hitKey(e) {
+  const key = document.querySelector(
+    `.player-button[data-button="${e.keyCode}"]`
+  );
+
+  let isSelected = document.querySelector("#player-select");
+  let finalSelection = options[e.keyCode];
+
+  if (!key) return;
+  key.classList.add("selected");
+
+  // display chosen object
+
+  if (e.keyCode in options) {
+    isSelected.innerHTML = `
+    <h5>Player chose <span class="text-warning text-capitalize">${finalSelection}</span></h5>
+    `;
+  }
 }
 
-function getPlayerChoice() {
-  return playerChoice.toLowerCase();
-}
+// function for computer selection
+function getComputerChoice(e) {
+  const rChoice = Object.keys(options);
+  let rSelected = rChoice[Math.floor(Math.random() * rChoice.length)];
 
-function playRound(playerSelection, computerSelection) {
-  playerSelection = playerSelection.toLowerCase();
-  computerSelection = getComputerChoice();
+  const cKey = document.querySelector(
+    `.computer-button[data-computer="${options[rSelected]}"]`
+  );
 
-  if (playerSelection === computerSelection) {
-    return "Tie game";
-  } else if (
-    (playerSelection === options[0] && computerSelection === options[2]) ||
-    (playerSelection === options[1] && computerSelection === options[0]) ||
-    (playerSelection === options[2] && computerSelection === options[1])
-  ) {
-    playerScore++;
-    return `You win! ${playerSelection} beats ${computerSelection}`;
+  let cSelected = document.querySelector("#computer-select");
+  let cFinal = options[rSelected];
+
+  if (!(e.keyCode in options)) {
+    return false;
   } else {
-    computerScore++;
-    return `You lose! ${computerSelection} beats ${playerSelection}`;
+    if (rSelected in options) {
+      cSelected.innerHTML = `
+      <h5>A.I. chose <span class="text-warning text-capitalize">${cFinal}</span></h5>
+      `;
+    }
   }
+
+  cKey.classList.add("selected");
 }
 
-function game() {
-  for (let i = 1; i <= 5; i++) {
-    let playerChoice = prompt("Choose between Rock, Paper, and Scissors");
-    let computerSelection = getComputerChoice();
-
-    console.log(
-      `Round ${i}: ${playRound(
-        playerChoice,
-        computerSelection
-      )}, Player Score: ${playerScore} vs Computer Score: ${computerScore}`
-    );
-  }
-
-  if (playerScore > computerScore) {
-    return "You Win";
-  } else if (playerScore < computerScore) {
-    return "You Lose";
-  } else {
-    return "Tie Game";
-  }
+// function to remove the effect when player changes choice
+function removeEffect(e) {
+  if (e.propertyName !== "transform") return;
+  this.classList.remove("selected");
 }
+
+// removing transition
+const keys = document.querySelectorAll(".player-button");
+keys.forEach((key) => key.addEventListener("transitionend", removeEffect));
+
+const cKeys = document.querySelectorAll(".computer-button");
+cKeys.forEach((key) => key.addEventListener("transitionend", removeEffect));
+
+// Event listener if player has selected
+window.addEventListener("keydown", hitKey);
+window.addEventListener("keydown", getComputerChoice);
